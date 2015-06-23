0cb0292ea77e109facd17afbad79e133593f3da3
Hinaser <rindo.hinase@gmail.com>
2015-06-24 00:00:00 +0900

    Crypto testing. (Successful pattern)

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+//Temporary
+using System.IO;
+using System.Security.Cryptography;
+//Temporary
+
 namespace PasswordManager
 {
     static class Program
@@ -14,10 +19,44 @@ namespace PasswordManager
         static void Main()
         {
             var password = Utility.GetHash("test");
-            var data = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            var data = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-            var encryptedData = Utility.Encrypt(data, password);
-            var decryptedData = Utility.Decrypt(encryptedData, password);
+            RijndaelManaged rijndael = new RijndaelManaged
+            {
+                Key = password,
+                Mode = CipherMode.CBC,
+                BlockSize = 128,
+                KeySize = 256,
+                Padding = PaddingMode.PKCS7
+            };
+            //rijndael.Key = password;
+
+            byte[] encryptedData;
+            byte[] decryptedData;
+
+            SymmetricAlgorithm algorithm = rijndael;
+
+            ICryptoTransform encryptor = algorithm.CreateEncryptor();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(data, 0, data.Length);
+                    cs.FlushFinalBlock();
+                    encryptedData = ms.ToArray();
+                }
+            }
+
+            ICryptoTransform decryptor = algorithm.CreateDecryptor();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(encryptedData, 0, encryptedData.Length);
+                    cs.FlushFinalBlock();
+                    decryptedData = ms.ToArray();
+                }
+            }
 
             return;
             //
